edd32df17f9708fdbb4a88efbdddb4454998563d
toti_lorca <1804140@alu.murciaeduca.es>
2024-03-01 12:20:57 +0100

    ldjkf

---
+++ python_2_trimestre/python_scripts_pc/23_paso_valor_referencia.py
@@ -0,0 +1,22 @@
+edad =21
+
+def incrementar_edad(edad):
+    #edad = edad + 1
+    edad+=1
+
+
+incrementar_edad(edad)
+print(edad)
+
+persona = {
+    'nombre' : 'Lidia',
+    'edad' : 21
+}
+
+def incrementar_edad(persona):
+    #persona['edad'] = persona['edad'] + 1
+    persona['edad'] += 1
+
+incrementar_edad(persona)
+print(persona)
+
